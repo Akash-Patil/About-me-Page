241b67ed17755f88a75e3b355fb7781a8510cd03
Jack <Jack@gmail.com>
2020-08-16 20:10:29 +0530

    First commit

---
+++ Projectaction.js
@@ -0,0 +1,41 @@
+
+
+"use strict";
+
+//for the scrolle feature 
+window.addEventListener('scroll', () =>{
+    const scrollable = document.documentElement.scrollHeight -window.innerHeight; //document - window
+    const scrolled = window.scrollY; //how much we have scrolled
+
+    if(Math.ceil(scrolled)===scrollable){
+        alert('Bottom of the page');
+    }
+
+
+});
+
+
+//for the show/hide button
+var show_hide = $("#show-hide");
+
+var toggleButton = $("#toggleButton");
+
+toggleButton.on("click", function() {
+    show_hide.toggle(500);
+        
+    if(toggleButton.text() == "Hide")
+    toggleButton.text("Show");
+
+    else
+    toggleButton.text("Hide");
+});
+
+
+//for the hover feature
+toggleButton.hover(function() {
+    $(this).css("background-color", "blue");
+}, function() {
+    $(this).css("background-color", "white");
+
+});
+
